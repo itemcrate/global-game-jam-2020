795e614368aa727195d8f223b70338fad89dc50c
learnb <learn.bryan@gmail.com>
2020-02-01 09:34:03 -0500

    test new creds

--- Objects/Enemy/Enemy.gd
+++ Objects/Enemy/Enemy.gd
@@ -12,12 +12,6 @@ var target = Vector2()
 func ai_sense_env():
 	target = Vector2(200,100)
 
-#func get_ai_input():
-#	velocity = Vector2()
-#	velocity.x += 1
-#	velocity.y += 1
-#	velocity = velocity.normalized() * speed 
-
 func ai_move_toward(_target):
 	velocity = _target - self.position
 	velocity = velocity.normalized()
