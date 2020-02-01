e634b3854773fe1b035780ba9539d550964dc5e1
RustyMonky <ian.cantor@jazzhr.com>
2020-02-01 16:03:56 -0500

    Implements decay bar to track decay timing; Fixes group typo and uses collider variable in player script

--- Objects/Obstruction/Barricade.gd
+++ Objects/Obstruction/Barricade.gd
@@ -2,7 +2,7 @@ extends "res://Objects/Obstruction/Obstruction.gd"
 
 func _ready():
 	# Override the parent values if required
-	decayTime = 5.00
+	decay_time = 5.00
 	._ready()
 
 func _on_Timer_timeout():
