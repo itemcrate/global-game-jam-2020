ee22a3b9fb5db3ff663459011e0bcc63ac247d58
Michael Springer <mike@sprngr.com>
2020-02-01 00:07:29 -0500

    Add player

---
+++ Objects/Player/Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+var motion: Vector2 = Vector2()
+var speed: int = 80
+
+func _ready():
+	pass
+	
+func _input(_event):
+	pass
+	
+func get_input():
+    # Detect up/down/left/right keystate and only move when pressed.
+    self.motion = Vector2()
+
+    if Input.is_action_pressed('player_right'):
+        self.motion.x += 1
+    if Input.is_action_pressed('player_left'):
+        self.motion.x -= 1
+    if Input.is_action_pressed('player_down'):
+        self.motion.y += 1
+    if Input.is_action_pressed('player_up'):
+        self.motion.y -= 1
+    self.motion = self.motion.normalized() * speed
+
+func _physics_process(delta):
+    get_input()
+    move_and_collide(self.motion * delta)
