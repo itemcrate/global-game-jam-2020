d61e362deae285925c30fc7e7a8070d989e92915
learnb <learn.bryan@gmail.com>
2020-02-02 00:15:30 -0500

    player does attack animation on action button

--- Objects/Player/Player.gd
+++ Objects/Player/Player.gd
@@ -9,6 +9,7 @@ var held_collectibles: Array = []
 var motion: Vector2 = Vector2()
 var speed: int = 80
 var animation = ""
+var attacking = false
 
 func _ready():
 	pass
@@ -38,7 +39,9 @@ func get_input():
 	self.motion = self.motion.normalized() * speed
 	
 	# Set Animation
-	if (self.motion.length()):
+	if attacking:
+		self._set_animation("Attack")
+	elif (self.motion.length()):
 		self._set_animation("Walk")
 	else:
 		self._set_animation("Idle")
@@ -61,6 +64,13 @@ func get_input():
 				self.held_collectibles = []
 	elif Input.is_action_just_released("player_action") && ray.is_colliding() && ray.get_collider().is_in_group("Obstructions"):
 		ray.get_collider().stopDamage()
+	
+	if Input.is_action_pressed("player_action"):
+		if !attacking:
+			self.attacking = true
+	
+	if Input.is_action_just_released("player_action"):
+		self.attacking = false
 
 func _set_animation(new_animation = ""):
 	if (self.animation == new_animation):
