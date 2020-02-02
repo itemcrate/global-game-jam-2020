70133c622994267241a34491a51ab858113753f5
Michael Springer <mike@sprngr.com>
2020-02-02 10:34:15 -0500

    reinitialize health so you may one day see victory

--- Screens/Title/Title.gd
+++ Screens/Title/Title.gd
@@ -7,4 +7,7 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
+		# Initialize Vehicle Health
+		WorldState.set_vehicle_health(30)
+	
 		Transition.fade_to("res://Screens/Main/Main.tscn")
